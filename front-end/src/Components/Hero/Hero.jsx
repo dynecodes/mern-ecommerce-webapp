8d913ae376d9502ba1ef2e2add6f94cd5c013400
dynecodes <dynechristelle01@gmail.com>
2025-08-11 18:38:06 +0800

    Update Hero.jsx

--- front-end/src/Components/Hero/Hero.jsx
+++ front-end/src/Components/Hero/Hero.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Hero.css';
-import hand_icon from '../Assets/hand_icon.png';
 import arrow_icon from '../Assets/arrow.png';
 import hero_image from '../Assets/hero_image.png';
 
@@ -10,12 +9,7 @@ const Hero = () => {
       <div className="hero-left">
         <h2>NEW ARRIVALS ONLY</h2>
         <div>
-          <div className="hero-hand-icon">
-            <p>new</p>
-            <img src={hand_icon} alt='' />
-          </div>
-          <p>collections</p>
-          <p>for everyone</p>
+          <p>FOR EVERYONE</p>
         </div> 
         <div className='hero-latest-btn'>
           <div>Latest Collection</div>
@@ -23,7 +17,7 @@ const Hero = () => {
         </div>
       </div>
       <div className="hero-right">
-        <img src={hero_image} alt="" />
+        <img src={hero_image} alt='' />
       </div>
     </div>
   );
