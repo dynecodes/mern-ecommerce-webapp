bd0807106364b501eb5ee4e6b3ac32a774ba367b
dynecodes <dynechristelle01@gmail.com>
2025-08-11 18:30:20 +0800

    Update Navbar.jsx

--- front-end/src/Components/Navbar/Navbar.jsx
+++ front-end/src/Components/Navbar/Navbar.jsx
@@ -19,31 +19,31 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='nav-logo'>
-        <img src={logo} alt='' />
-        <p>SHOPPER</p>
+        <img src={logo} alt='Logo' />
+        <p>cartzy</p>
       </div>
       <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt='' />
       <ul ref={menuRef} className='nav-menu'>
         <li onClick={() => setMenu('shop')}>
-          <Link style={{ textDecoration: 'none' }} to='/'>
+          <Link style={{ textDecoration: 'none', color: 'black' }} to='/'>
             Shop
           </Link>
           {menu === 'shop' && <hr />}
         </li>
         <li onClick={() => setMenu('mens')}>
-          <Link style={{ textDecoration: 'none' }} to='/mens'>
+          <Link style={{ textDecoration: 'none', color: 'black'}} to='/mens'>
             Men
           </Link>
           {menu === 'mens' && <hr />}
         </li>
         <li onClick={() => setMenu('womens')}>
-          <Link style={{ textDecoration: 'none' }} to='/womens'>
+          <Link style={{ textDecoration: 'none', color: 'black' }} to='/womens'>
             Women
           </Link>
           {menu === 'womens' && <hr />}
         </li>
-        <li onClick={() => setMenu('kids')}>
-          <Link style={{ textDecoration: 'none' }} to='/kids'>
+        <li onClick={() => setMenu('kids' )}>
+          <Link style={{ textDecoration: 'none', color: 'black'}} to='/kids'>
             Kids
           </Link>
           {menu === 'kids' && <hr />}
