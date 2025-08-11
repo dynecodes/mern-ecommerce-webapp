da383d66aa5e3f142eb36b5da4da8d2bd5801783
dynecodes <dynechristelle01@gmail.com>
2025-08-11 19:31:48 +0800

    Update NewCollections.jsx

--- front-end/src/Components/NewCollections/NewCollections.jsx
+++ front-end/src/Components/NewCollections/NewCollections.jsx
@@ -8,7 +8,7 @@ const NewCollections = () => {
   const [new_collection,setNew_collection] = useState([]);
 
   useEffect(()=>{
-    fetch('https://e-commerce-react-xp0f.onrender.com/new-collections')
+    fetch('https://mern-ecommerce-webapp-back-end.onrender.com/new-collections')
     .then((response)=>response.json())
     .then((data)=>setNew_collection(data));
   },[])
