7b15bad5fcfb3f85b1bd7bd4e411a51bb9b636e2
dynecodes <dynechristelle01@gmail.com>
2025-08-13 15:10:42 +0800

    Update ListProduct.jsx

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import './ListProduct.css';
 import cross_icon from '../../assets/cross_icon.png';
 
-const backend_url = import.meta.env.VITE_BASE_URL;
 
 const ListProduct = () => {
   const [allproducts, setAllProducts] = useState([]);
@@ -45,7 +44,7 @@ const ListProduct = () => {
         {allproducts.map((product, index) => {
           return (
             <div key={index} className="listproduct-format-main listproduct-format">
-              <img src={`${backend_url}${product.image}`} alt="" className="listproduct-product-icon" />
+              <img src={`https://mern-ecommerce-webapp-back-end.onrender.com${product.image}`} alt="" className="listproduct-product-icon" />
               <p>{product.name}</p>
               <p>${product.old_price}</p>
               <p>${product.new_price}</p>
