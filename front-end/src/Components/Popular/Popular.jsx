93b96910190203a25cd72af32c78a48b57d12e92
dynecodes <dynechristelle01@gmail.com>
2025-08-13 15:19:26 +0800

    Update Popular.jsx

--- front-end/src/Components/Popular/Popular.jsx
+++ front-end/src/Components/Popular/Popular.jsx
@@ -8,7 +8,7 @@ const Popular = () => {
   const [popularProducts,setPopularProducts] = useState([]);
 
   useEffect(()=>{
-    fetch('https://e-commerce-react-xp0f.onrender.com/popularinwomen')
+    fetch('https://mern-ecommerce-webapp-back-end.onrender.com/popularinwomen')
     .then((response)=>response.json())
     .then((data)=>setPopularProducts(data));
   },[])
