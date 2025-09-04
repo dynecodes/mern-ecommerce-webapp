b1b5937e85dcc410335c4d85f069eac4c9dfc9f8
dynecodes <dynechristelle01@gmail.com>
2025-09-04 17:19:59 +0800

    Update Footer.jsx

--- front-end/src/Components/Footer/Footer.jsx
+++ front-end/src/Components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <div className='footer'>
       <div className="footer-logo">
         <img src={footer_logo} alt=''/>
-        <p>SHOPPER</p>
+        <p>cartzy</p>
       </div>
       <ul className="footer-links">
         <li>Company</li>
