4294f00ef034da0b40217a983af7cb9ff835c4b9
dynecodes <dynechristelle01@gmail.com>
2025-12-05 13:15:21 +0800

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -253,5 +253,5 @@ app.post('/getcart', fetchUser, async (req, res) => {
 
 
 // Start the server
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
